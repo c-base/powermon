eef5ce8c76de871d88aab7b324c39f7154c7b591
Andreas 'count' Kotes <count@c-base.org>
2016-03-10 00:17:13 +0100

    cosmetics / maintainability / race condition

--- graphloop.sh
+++ graphloop.sh
@@ -2,6 +2,28 @@
 
 while inotifywait -qq -e close_write powermon.rrd; do
     for PERIOD in 1hour 4hours 1day 3days 1week 1month 1year ; do
-        rrdtool graph powermon-${PERIOD}.png --end now --start end-${PERIOD} DEF:min=powermon.rrd:load:MIN DEF:max=powermon.rrd:load:MAX DEF:avg=powermon.rrd:load:AVERAGE --color 'SHADEA#425466' --color 'SHADEB#425466' --color 'GRID#ffffff7f' --color 'MGRID#FF99007f' --color 'BACK#22374C' --color 'FRAME#000000' --color 'FONT#ffffff' --color 'CANVAS#22374C' --color 'ARROW#FFFFFF' -w 640 -h 150 -t "spacestation power consumption" -v "Watt" "COMMENT:Watt used within the last ${PERIOD}" 'AREA:avg#000000:Average' 'LINE1:min#46B6EE:Min' 'LINE1:max#FF9900:Max' >/dev/null
+        rrdtool graph powermon-${PERIOD}.png.new \
+        --start end-${PERIOD} \
+        --end now \
+        DEF:min=powermon.rrd:load:MIN \
+        DEF:max=powermon.rrd:load:MAX \
+        DEF:avg=powermon.rrd:load:AVERAGE \
+        --color 'SHADEA#425466' \
+        --color 'SHADEB#425466' \
+        --color 'GRID#ffffff7f' \
+        --color 'MGRID#FF99007f' \
+        --color 'BACK#22374C' \
+        --color 'FRAME#000000' \
+        --color 'FONT#ffffff' \
+        --color 'CANVAS#22374C' \
+        --color 'ARROW#FFFFFF' \
+        -w 640 -h 150 \
+        -t "spacestation power consumption" \
+        -v "Watt" \
+        "COMMENT:Watt used within the last ${PERIOD}" \
+        'AREA:avg#000000:Average' \
+        'LINE1:min#46B6EE:Min' \
+        'LINE1:max#FF9900:Max' \
+        >/dev/null && mv powermon-${PERIOD}.png.new powermon-${PERIOD}.png
     done
 done
